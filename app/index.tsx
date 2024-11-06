bc06ab91484b20aeb3a57148e6f608693a4996b8
tyagidevansh <devanshtyagi0607@gmail.com>
2024-11-06 23:34:47 +0530

    native wind

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Image, StyleSheet, Platform, View, Text } from "react-native" 
+
+export default function HomeScreen() {
+  return (
+    <View className="mt-16">
+      <Text className="text-red-500">Hello World</Text>
+    </View>
+  );
+}
